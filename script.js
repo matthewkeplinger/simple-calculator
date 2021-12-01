91e871f479f0f2eb5d66d9712372737e3815208f
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-12-01 09:55:49 -0500

    Compute function finished and working

--- script.js
+++ script.js
@@ -33,7 +33,30 @@ class Calculator{
     }
 
     compute(){
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
 
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '/':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     }
 
     updateDisplay(){
@@ -68,4 +91,10 @@ operationButtons.forEach(button =>{
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+//event listener for equals button
+equalsButton.addEventListener('click', button =>{
+    calculator.compute();
+    calculator.updateDisplay();
 })
